323dcdba38421c0d02d2c7f8fc7c0aec6e2e38a1
ArmicustosRK <26887047+KyleKKAng@users.noreply.github.com>
2019-04-01 01:02:23 -0400

    two sum java

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -5,13 +5,7 @@ public class TwoSum {
 
     You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-Example:
-
-    Given nums = [2, 7, 11, 15], target = 9,
-
-    Because nums[0] + nums[1] = 2 + 7 = 9,
-    return [0, 1].
-     */
+    */
     public int[] twoSum(int[] nums, int target){
         if (nums == null || nums.length < 2) {
             return new int[]{0,0};
