e7c0801b0a6053eb99df9b1502d333fe6603e4f4
ArmicustosRK <26887047+KyleKKAng@users.noreply.github.com>
2019-04-01 22:58:54 -0400

    two sum java

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -8,9 +8,9 @@ public class TwoSum {
     */
     public int[] twoSum(int[] nums, int target){
         if (nums == null || nums.length < 2) {
-            return new int[]{0,0};
+            return new int[]{-1,-1};
         }
-        int tmp[] = new int[]{0,0};
+        int tmp[] = new int[]{-1,-1};
         Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++){
             if (map.containsKey(target - nums[i])){
@@ -20,9 +20,8 @@ public class TwoSum {
             }
             map.put(nums[i], i);
         }
-
         return tmp;
     }
 
-}
 
+}
